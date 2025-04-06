f45b4001a20133d51893f51a13ff4ee729100050
jesusmh <eduardoomh@outlook.com>
2025-04-06 16:40:04 -0500

    Company page updates

--- src/components/trustedCompanies/CompaniesList.tsx
+++ src/components/trustedCompanies/CompaniesList.tsx
@@ -43,27 +43,37 @@ const companies = [
     },
 ];
 
-export default function CompaniesList() {
+interface Props {
+    exclude?: string;
+}
+
+export default function CompaniesList(props: Props) {
     const intinityCompanies = [...companies, ...companies, ...companies]
+    const { exclude = 'none' } = props
+
     return (
         <div className={styles.companiesList}>
             <div className={styles.sliderCompanies}>
-        
-            {
-                intinityCompanies.map(item => (
-                    <article className={styles.companyItem}>
-                        <a href={item.link}>
-                            <img
-                                src={item.image}
-                                alt={item.company}
-                                width="287"
-                                height="157"
-                            />
-                            <h3 className={styles.companyItem_title}>{item.company}</h3>
-                        </a>
-                    </article>
-                ))
-            }
+                {
+                    intinityCompanies
+                        .filter(item => item.company !== exclude)
+                        .map((item, index) => (
+                            <article className={styles.companyItem} key={`${item.company}-${index}`}>
+                                <a
+                                    href={item.link}
+                                    ref={(el) => (el as HTMLAnchorElement | null)?.setAttribute('viewTransition', '')}
+                                >
+                                    <img
+                                        src={item.image}
+                                        alt={item.company}
+                                        width="287"
+                                        height="157"
+                                    />
+                                    <h3 className={styles.companyItem_title}>{item.company}</h3>
+                                </a>
+                            </article>
+                        ))
+                }
             </div>
         </div>
     )
