69708d0023cb11db806f6701751e05ff883972a6
jesusmh <eduardoomh@outlook.com>
2025-06-11 16:27:19 -0500

    Updating things

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,7 @@ import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 //import vercel from "@astrojs/vercel";
 
+
 export default defineConfig({
     integrations: [react()],
     base: '/en',
