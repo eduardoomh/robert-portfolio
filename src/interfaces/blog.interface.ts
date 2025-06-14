192ed9409a7343f734c3d83463ce161cb771555b
jesusmh <eduardoomh@outlook.com>
2025-06-14 13:12:37 -0500

    Blog posts added

---
+++ src/interfaces/blog.interface.ts
@@ -0,0 +1,14 @@
+
+interface PostI{
+    id: number;
+    date: string;
+    slug: string;
+    status: string;
+    title: string;
+    description: string;
+    content: string;
+    excerpt: string;
+    author: string;
+    thumbnail: string;
+    keywords: string[];
+}
